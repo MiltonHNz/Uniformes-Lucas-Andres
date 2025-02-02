99c8029291347d89d16a2ca745420215c81eb957
MiltonH <gh498149@gmail.com>
2025-02-01 20:22:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ function navigateTo(section) {
     window.location.href = section + ".html";
 }
 
-// Función para cerrar el mensaje emergente
+function openContact() {
+    alert("Formulario de contacto próximamente disponible.");
+}
+
 function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
 
-// Mostrar el mensaje emergente al cargar la página
 window.onload = function() {
     let popup = document.getElementById("popup");
     if (popup) {
